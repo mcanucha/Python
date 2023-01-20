10d827c0c5eeef71e56b63cc20236d00a1e1558a
mcanucha <120558697+mcanucha@users.noreply.github.com>
2023-01-20 08:25:10 +0100

    first commit

---
+++ Quiz.py
@@ -0,0 +1,24 @@
+Greet and welcome the user
+Ask for user input and store as variable
+Write the question
+Write the answer
+
+
+
+
+
+
+
+
+print ("Hello, welcome to my Quizgame.")
+
+print ("Question 1. What is the capital of Nigeria?")
+userInput = input()
+
+if (userInput.lower() == "Abuja".lower()):
+
+	print("Correct!! Congratulations")
+
+else:
+
+	print("Sorry, the correct answer is Abuja.")
---
+++ Rainfall.py
@@ -0,0 +1,6 @@
+acre= 43560 # square feet
+inch_of_rain =1
+gallons_per_sq_feet = 0.623
+total_gallons= acre * inch_of_rain * gallons_per_sq_feet
+
+print(total_gallons)
